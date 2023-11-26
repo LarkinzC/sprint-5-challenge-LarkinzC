63e39648910015f0360d1da2d9ebc9fa076302c9
Larkin Campbell <Larkincampbell24@gmail.com>
2023-11-25 17:47:21 -0800

    progress babay

--- frontend/index.js
+++ frontend/index.js
@@ -1,41 +1,68 @@
 
-async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
-  // 👇 WORK WORK BELOW THIS LINE 
-    try {
-      const mentorsURL = ('http://localhost:3003/api/mentors') 
-      const learnersURL =('http://localhost:3003/api/learners') 
-      const resLearn = await axios.get(learnersURL)
-      const resMent = await axios.get(mentorsURL)
-      console.log(resLearn.data)
-      console.log(resMent.data)
-      resLearn.data.forEach(({mentors}) => {
-        mentors.forEach((mentorId , idx) => {
-          const {firstName, lastName} = resMent.data.find(({id}) => 
-            id === mentorId
-          )
-          console.log(firstName, lastName)
-          mentors[idx] = `${firstName} ${lastName}`
-        })
-      })
-    } catch (error) {
-      console.log(error.message)
-    }
+async function sprintChallenge5() {
+  try {
+    const mentorsURL = 'http://localhost:3003/api/mentors';
+    const learnersURL = 'http://localhost:3003/api/learners';
+    const resLearn = await axios.get(learnersURL);
+    const resMent = await axios.get(mentorsURL);
+
+    const container = document.querySelector('.cards');
     
-  function learnerCard(learner) {
+    resLearn.data.forEach(({ mentors }) => {
+      mentors.forEach((mentorId, idx) => {
+        const { firstName, lastName } = resMent.data.find(({ id }) => id === mentorId);
+        mentors[idx] = `${firstName} ${lastName}`;
+      });
+    });
 
+    return resLearn.data;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
   }
+}
 
+function learnerCard(learner, container) {
+  const card = document.createElement('div');
+  card.classList.add('card');
 
+  const learnerName = document.createElement('h3');
+  learnerName.textContent = 'Name: ' + learner.fullName;
 
+  const learnerEmail = document.createElement('div');
+  learnerEmail.textContent = 'Email: ' + learner.email;
 
+  const mentorDropLi = document.createElement('h4');
+  mentorDropLi.classList.add('open');
+  mentorDropLi.textContent = 'Mentors: ' + learner.mentors.join(', ');
 
+  card.appendChild(learnerName);
+  card.appendChild(learnerEmail);
+  card.appendChild(mentorDropLi);
 
+  container.appendChild(card);
+}
 
+async function someAsyncFunction() {
+  try {
+    const processedData = await sprintChallenge5();
 
+    const container = document.querySelector('.cards');
 
+    processedData.forEach((learner) => {
+      learnerCard(learner, container);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-   }
+someAsyncFunction();
+
   
+   
+
+
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
